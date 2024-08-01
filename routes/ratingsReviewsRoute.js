7505ac33ac8878a49caf2d273aea537d9799cabb
$@#il <sahilsadekar249775@gmail.com>
2024-08-01 18:15:03 +0530

    imporved initial review api working

--- routes/ratingsReviewsRoute.js
+++ routes/ratingsReviewsRoute.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {createReview,getReviewsInformations}=require('../controller/reviewsController');
+const path = require('path');
+const { createReview, getReviewsInformations } = require('../controller/reviewsController');
+
+//mukter imports
+const multer = require('multer')
+const upload = multer({
+    dest: path.join(__dirname, '../uploads')
+});
 //all routes related to the revies and  ratings section
-router.post("/create-review", createReview);
+router.post("/create-review", upload.single('imageFromCust'), createReview);
 //get all data related to the revies and ratings section
 router.get("/reviews-informations/:pId", getReviewsInformations);
 module.exports = router;
